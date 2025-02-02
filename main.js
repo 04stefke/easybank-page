218d4621182af7485073033c7ddc38d3ec36e088
04stefke <jovanovics399@yahoo.com>
2025-02-02 12:15:52 +0000

    removed the main content changed the position of dropdown when returning

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function onToggleMenu(e) {
 	let menu = document.getElementById("mobile-menu");
 
-	menu.classList.toggle("top-[10%]");
+	menu.classList.toggle("top-[110px]");
 
-	e.src = menu.classList.contains("top-[10%]")
+	e.src = menu.classList.contains("top-[110px]")
 		? "./images/icon-close.svg"
 		: "./images/icon-hamburger.svg";
 }
